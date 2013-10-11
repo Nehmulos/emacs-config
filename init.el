6779e0d1703be5689759c57d773b734fb6444b4a
Nehmulos <nehmu@lavabit.com>
2013-10-11 12:06:10 +0200

    added linum and oblivion theme;

--- init.el
+++ init.el
@@ -1,2 +1,14 @@
 (setq tab-width 4)
 (setq indent-tabs-mode nil)
+
+;; oblivion theme custom added
+;;(load-file "~/.emacs.d/load-directory.el")
+;;(require 'load-directory)
+;;(load-directory "~/.emacs.d/oblivion-emacs")
+;;(load-file "~/.emacs.d/oblivion-emacs/color-theme-oblivion.el")
+(load-file "~/.emacs.d/oblivion-emacs/color-theme.el")
+(load-file "~/.emacs.d/oblivion-emacs/color-theme-oblivion.el")
+(color-theme-oblivion)
+
+;; lines on the left
+(load-file "~/.emacs.d/linum.el")
