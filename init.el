f168fa4947ec86eaa853900f085913db2a23ea90
Nehmulos <nehmu@lavabit.com>
2013-11-09 15:09:35 +0100

    use 4 spaces for tabs in c-mode; autoclose firststart help screen;

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (setq tab-width 4)
 (setq indent-tabs-mode nil)
+(setq c-basic-offset 4)
 
 ;; oblivion theme custom added
 ;;(load-file "~/.emacs.d/load-directory.el")
@@ -15,3 +16,15 @@
 (linum-mode)
 
 (load-file "~/.emacs.d/rust-mode.el")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
